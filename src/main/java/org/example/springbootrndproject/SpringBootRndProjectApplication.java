e84be2f93e99b6507e07577fe4bca8c847e6f35a
shivakant singh <shivakant5875@gmail.com>
2024-12-03 07:35:21 +0530

    nothing to commit

---
+++ src/main/java/org/example/springbootrndproject/SpringBootRndProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootrndproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRndProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRndProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbootrndproject/SpringBootRndProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootrndproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRndProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
